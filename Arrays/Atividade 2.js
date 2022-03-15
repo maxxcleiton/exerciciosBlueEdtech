c11ed564ef3fcfba5d0537ea8bd3b0886cf86592
Max Cleiton <maxcleiton@live.com>
2022-03-15 20:04:28 -0300

    Um programa que peça idade E altura

--- Arrays/Atividade 2.js
+++ Arrays/Atividade 2.js
@@ -5,20 +5,39 @@ console.log();
 // Atividade 2
 // Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo array. Imprima a média de idade e de altura.
 
+// Faça um Programa que peça a idade e a altura de 5 pessoas:
+
 let idade = []
 let altura = []
 let contador = 0;
+let contador2 = 0;
 let i = 0;
+let i2 = 0;
 
 do {
     i = +prompt('Qual sua idade: ');
     idade.push(i);
-    console.log(i);
-    console.log(idade);
     contador++
-} while (contador < 5)
+} while (contador < 5);
 
 console.log();
-console.log(idade);
 
-// Saindo como undefined, preciso resolver
+do {
+    i2 = 0;
+    i2 = +prompt('Qual sua altura: ');
+    altura.push(i2);
+    contador2++
+} while (contador2 < 5);
+
+// Imprima a média de idade e de altura.
+// média é a soma de todos os x, dividido por x
+
+
+// Dados à mostra:
+// console.log();
+// console.log(`Lista de idades: ${idade}`);
+// console.log();
+// console.log(`Lista de alturas: ${altura}`);
+// console.log();
+
+// Max Cleiton
