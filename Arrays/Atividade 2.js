713cbe5e9ef8872c4617d29a81e4f6b4cd7a2e20
Max Cleiton <maxcleiton@live.com>
2022-03-17 15:29:48 -0300

    Código mais simples e metodo passo-a-passo

--- Arrays/Atividade 2.js
+++ Arrays/Atividade 2.js
@@ -7,27 +7,27 @@ console.log();
 
 // Faça um Programa que peça a idade e a altura de 5 pessoas:
 
-let idade = []
-let altura = []
-let contador = 0;
-let contador2 = 0;
-let i = 0;
-let i2 = 0;
+// let idade = []
+// let altura = []
+// let contador = 0;
+// let contador2 = 0;
+// let i = 0;
+// let i2 = 0;
 
-do {
-    i = +prompt('Qual sua idade: ');
-    idade.push(i);
-    contador++
-} while (contador < 5);
+// do {
+//     i = +prompt('Qual sua idade: ');
+//     idade.push(i);
+//     contador++
+// } while (contador < 5);
 
-console.log();
+// console.log();
 
-do {
-    i2 = 0;
-    i2 = +prompt('Qual sua altura: ');
-    altura.push(i2);
-    contador2++
-} while (contador2 < 5);
+// do {
+//     i2 = 0;
+//     i2 = +prompt('Qual sua altura: ');
+//     altura.push(i2);
+//     contador2++
+// } while (contador2 < 5);
 
 // Imprima a média de idade e de altura.
 // média é a soma de todos os x, dividido por x
@@ -39,5 +39,18 @@ do {
 // console.log();
 // console.log(`Lista de alturas: ${altura}`);
 // console.log();
+
+let idade = [];
+idade = prompt('Qual sua idade? ');
+
+let altura = [];
+altura = prompt('Qual sua altura? ');
+
+for (let i = 0; i < 5; i++){
+    
+}
+
+console.log(idade, altura)
+
 
 // Max Cleiton
