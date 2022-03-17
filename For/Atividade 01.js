b9a08a1bed26a85319bf79a3a1aa65be92369a5f
Max Cleiton <maxcleiton@live.com>
2022-03-17 15:30:15 -0300

    Início

---
+++ For/Atividade 01.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+console.clear();
+
+// Atividade 1
+// Peça ao usuário uma quantidade de linhas e e outra de colunas, e imprima essas informações na forma de uma tabela como no exemplo de 3 linhas e 4 colunas:
+// [ 0, 1, 2, 3 ]
+// [ 1, 2, 3, 4 ]
+// [ 2, 3, 4, 5 
