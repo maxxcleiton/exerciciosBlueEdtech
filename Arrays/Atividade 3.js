242a7e53b2bba5fdb63bbec1a9bf2e9b596ff0e9
Max Cleiton <maxcleiton@live.com>
2022-03-17 15:54:22 -0300

    Início

---
+++ Arrays/Atividade 3.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+console.clear()
+console.log();
+
+// Atividade 3
+// Faça um programa em que o usuário escolha a quantidade de alunos a serem cadastrados e peça o nome e nota de cada um. Armazene as informações em duas listas separadas e no final imprima o nome e a nota correspondente de cada aluno, e a média total da notas.
+
