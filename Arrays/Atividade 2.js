e8c353f98158e07a547028cbe8818f61f6832b9b
Max Cleiton <maxcleiton@live.com>
2022-03-17 15:52:20 -0300

    Básico funcionando

--- Arrays/Atividade 2.js
+++ Arrays/Atividade 2.js
@@ -5,52 +5,35 @@ console.log();
 // Atividade 2
 // Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo array. Imprima a média de idade e de altura.
 
-// Faça um Programa que peça a idade e a altura de 5 pessoas:
-
-// let idade = []
-// let altura = []
-// let contador = 0;
-// let contador2 = 0;
-// let i = 0;
-// let i2 = 0;
-
-// do {
-//     i = +prompt('Qual sua idade: ');
-//     idade.push(i);
-//     contador++
-// } while (contador < 5);
-
-// console.log();
-
-// do {
-//     i2 = 0;
-//     i2 = +prompt('Qual sua altura: ');
-//     altura.push(i2);
-//     contador2++
-// } while (contador2 < 5);
-
-// Imprima a média de idade e de altura.
-// média é a soma de todos os x, dividido por x
-
-
-// Dados à mostra:
-// console.log();
-// console.log(`Lista de idades: ${idade}`);
-// console.log();
-// console.log(`Lista de alturas: ${altura}`);
-// console.log();
-
-let idade = [];
-idade = prompt('Qual sua idade? ');
-
+let idade = []; // Arrays principal
 let altura = [];
-altura = prompt('Qual sua altura? ');
 
-for (let i = 0; i < 5; i++){
-    
+let idadeN = []; // 'N' - Number
+let alturaN = [];
+
+let idadeM = 0; // 'M' - Média
+let alturaM = 0;
+
+for (let i = 0; i < 5; i++){ // Contador com as perguntas
+    idadeN = +prompt('Qual sua idade? ');
+    idade.push(idadeN);
+    alturaN = +prompt('Qual sua altura? ');
+    altura.push(alturaN);
 }
 
 console.log(idade, altura)
 
+idadeM = idade[0] + idade[1] + idade[2] + idade [3] + idade[4] / 5
+
+console.log(idadeM)
+
+alturaM = altura[0] + altura[1] + altura[2] + altura[3] + altura[4] / 5
+
+console.log(alturaM)
+
+// O código está pronto, falta perfurmar
+// Penso em adicionar um contador nos indices das médias
+// altura[i] <- i vai de 0 a i < 5
+// assim dispensa tanto código
 
 // Max Cleiton
