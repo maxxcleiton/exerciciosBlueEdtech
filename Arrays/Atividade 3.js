f422971d3f42e97223be4ea1a4f9e974bd55d776
Max Cleiton <maxcleiton@live.com>
2022-03-20 15:32:15 -0300

    Básico iniciado

--- Arrays/Atividade 3.js
+++ Arrays/Atividade 3.js
@@ -5,3 +5,15 @@ console.log();
 // Atividade 3
 // Faça um programa em que o usuário escolha a quantidade de alunos a serem cadastrados e peça o nome e nota de cada um. Armazene as informações em duas listas separadas e no final imprima o nome e a nota correspondente de cada aluno, e a média total da notas.
 
+// Quantidade de alunos
++prompt('Escolha a quantidade de alunos: '); //Tem que dar um numero que será o numero de repetição dos seguintes:
+
++prompt('Qual o nome do aluno? ');
++prompt('Qual a nota do aluno?');
+
+let qt = 0; // Quantidade de alunos
+let nm = []; // Nome do aluno
+let nt = []; // Nota do aluno
+
+console.log(); // Nome e a nota de cada aluno
+console.log('A média total das notas dos alunos é: '); // Média total das notas
