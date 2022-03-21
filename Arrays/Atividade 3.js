62b293b82d7d17a91a08dc44adeba0c3e4216d3b
Max Cleiton <maxcleiton@live.com>
2022-03-20 22:26:40 -0300

    Adição de strings de arrumação e créditos finais

--- Arrays/Atividade 3.js
+++ Arrays/Atividade 3.js
@@ -12,20 +12,28 @@ let nt = []; // Nota do aluno
 
 // Quantidade de alunos
 qt = +prompt('Escolha a quantidade de alunos: '); // Tem que dar um numero que será o numero de repetição dos seguintes:
+console.log();
 
 for (let i = 0; i < qt; i++) { // Repetição das perguntas
     nm.push(prompt('Qual o nome do aluno? '));
     nt.push(+prompt('Qual a nota do aluno? '));
-
+    console.log();
 }
-
+console.log('---------------');
 for (let i = 0; i < qt; i++) { // Repetição do índice da quantidade das arrays nm e nt
     console.log(`Aluno: ${nm[i]}\nNota: ${nt[i]}`);
+    console.log();
     }
 
 let soma = 0;
 for (let sm of nt) { // Vai retornar todos os elementos somando cada um com 0, totalizando a soma de todos
     soma = soma + sm // Soma é o total das notas
 }
+console.log('---------------');
+console.log('A média total das notas dos alunos é: ', (soma / qt)); // Média, utilizando as arrays que viraram uma soma usando for of, dividido pela quantidade de alunos (qt)
 
-console.log('A média total das notas dos alunos é: ', (soma / qt)); // Média total das notas
+// Fim de código.
+
+// Max Cleiton.
+// https://github.com/maxxcleiton
+// https://www.linkedin.com/in/max-cleiton-4874a21b0/
