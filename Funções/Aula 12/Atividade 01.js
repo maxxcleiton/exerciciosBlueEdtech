8580173fd88e48b4db1706d11e0a491eb006e64c
Max Cleiton <maxcleiton@live.com>
2022-03-22 01:32:27 -0300

    Primeiro exercício de função, aprendi graças a ele

---
+++ Funções/Aula 12/Atividade 01.js
@@ -0,0 +1,23 @@
+const prompt = require('prompt-sync')();
+console.log();
+
+// Atividade 1 - Aula 12
+// Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores.
+
+function operacoes(num1, num2){
+    console.log(`A soma dos dois valores é: ${num1 + num2}`);
+    console.log(`A subtração dos dois valores é: ${num1 - num2}`);
+    console.log(`A multiplicação dos dois valores é: ${num1 * num2}`);
+    console.log(`A divisão dos dois valores é: ${num1 / num2}`);
+}
+
+let n1 = +prompt('Digite o primeiro número: ');
+let n2 = +prompt('Digite o segundo número: ');
+
+operacoes(n1,n2);
+
+// Fim de código.
+
+// Max Cleiton.
+// https://github.com/maxxcleiton
+// https://www.linkedin.com/in/max-cleiton-4874a21b0/
