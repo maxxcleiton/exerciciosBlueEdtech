dfdd32b70b92f54cd6787d040dfdcaf0460c3f2a
Max Cleiton <maxcleiton@live.com>
2022-03-22 01:51:04 -0300

    Início

---
+++ Funções/Atividade 01-Aula 11.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+console.clear();
+console.log();
+
+// Atividade 1 - Aula 11
+// Faça um programa, com uma função que necessite de três argumentos: dois números e um sinal de operador matemático (+, -, * ou /). Ela deve fazer o cálculo indicado pelo operador usando os dois número passados.
+
+function f (x,y,z) {
+   let z = '+'
+    x z y;
+
+    return calculo
+}
+
+f(12,8,z)
+
+// Concluir ainda
