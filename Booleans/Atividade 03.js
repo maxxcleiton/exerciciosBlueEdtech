0b147496dcf1fcb0decd52e48162a0bae2825634
Max Cleiton <maxcleiton@live.com>
2022-03-22 22:50:32 -0300

    Atividade concluída

---
+++ Booleans/Atividade 03.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+console.clear();
+
+// Atividade 3
+// Faça um programa que leia um número, e informe se ele é par ou impar
+
+let number = +prompt("Insira um número: ");
+
+if (number % 2 == 0) {
+    console.log("É par.");
+} else {
+    console.log("É ímpar.");
+}
+
+
+// Fim de código.
+
+// Max Cleiton.
+// https://github.com/maxxcleiton
+// https://www.linkedin.com/in/max-cleiton-4874a21b0/
