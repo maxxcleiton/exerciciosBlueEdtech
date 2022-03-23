14a2559333a7619b14c2fbd85428d3e74bb901c4
Max Cleiton <maxcleiton@live.com>
2022-03-23 00:42:54 -0300

    Tentativa falha da operacao na funcao

--- Funções/Atividade 01-Aula 11.js
+++ Funções/Atividade 01-Aula 11.js
@@ -6,12 +6,11 @@ console.log();
 // Faça um programa, com uma função que necessite de três argumentos: dois números e um sinal de operador matemático (+, -, * ou /). Ela deve fazer o cálculo indicado pelo operador usando os dois número passados.
 
 function f (x,y,z) {
-   let z = '+'
-    x z y;
+   calculo = xzy
 
     return calculo
 }
 
-f(12,8,z)
+f(12,8,+)
 
 // Concluir ainda
