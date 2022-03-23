3d39a4cf808bc23a3d3e8d0315431cecb90fc55d
Max Cleiton <maxcleiton@live.com>
2022-03-22 22:42:35 -0300

    Correçoes e enfeites

--- Booleans/Atividade 01.js
+++ Booleans/Atividade 01.js
@@ -5,13 +5,24 @@ console.clear();
 
 // Faça um Programa que peça dois números e imprima o maior deles, e informe caso eles sejam iguais.
 
-let number1 = +prompt('Digite um numero ')
-let number2 = +prompt('Digite outro numero ')
+console.log();
+let number1 = +prompt('Digite um número: ')
+let number2 = +prompt('Digite outro número: ')
 
 if (number1 > number2) {
-    console.log("O numero 1 é maior que o 2");
+    console.log();
+    console.log("O primeiro número é maior que o segundo.");
 } else if (number2 > number1) {
-    console.log("O numero 2 é maior que o 1");
+    console.log();
+    console.log("O segundo número é maior que o primeiro.");
 } else if (number1 == number2) {
-    console.log("Os dois números são iguais");
+    console.log();
+    console.log("Os dois números são iguais.");
 }
+
+
+// Fim de código.
+
+// Max Cleiton.
+// https://github.com/maxxcleiton
+// https://www.linkedin.com/in/max-cleiton-4874a21b0/
