9d3fdf0fd5f4f60e7b23bcafd3124542e3aebb6f
Max Cleiton <maxcleiton@live.com>
2022-03-28 00:30:18 -0300

    Início

---
+++ Funções/Atividade 2.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+console.clear();
+console.log();
+
+// Atividade 2
+// Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato 'D de mesPorExtenso de AAAA'. Valide a data e retorne NULL caso a data seja inválida.
+
+function d(d,m,a){
+    
+}
