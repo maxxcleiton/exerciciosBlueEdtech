34a35681dd1186fc6bcc20217da330233d2a9f86
Max Cleiton <maxcleiton@live.com>
2022-03-28 00:31:06 -0300

    alteracoes e testes

--- Objetos/Atividade 2.js
+++ Objetos/Atividade 2.js
@@ -4,21 +4,42 @@ console.log();
 // Atividade 2
 // Crie um sistema de cadastro de funcionários que deve criar um objeto novo para cada funcionário cadastrado com as informações: nome, cargo e salário. Ele deve armazenar todos esses objetos em uma lista. Ao final de cada cadastro ele deve perguntar se deseja cadastrar um novo funcionário e recomeçar o processo, e caso não haja mais funcionários para cadastrar, ele deve chamar uma função que deve apresentar a quantidade de funcionários com cada cargo e os nomes deles. (Você pode limitar as opções de cargo para facilitar.)
 
-// Cadastro de funcionarios
 
-// Funcao
-function nf(funcionario){ //novo funcionario
-    let nomeFun = prompt(`Digite o nome do funcionário: `);
-    let cargoFun = prompt(`Digite o cargo do funcionário: `);
-    let salarioFun = +prompt(`Digite o salário do funcionário: `);
-    let obfuncionario = {
-        Nome: nomeFun,
-        Cargo: cargoFun,
-        Salario: salarioFun,
-    }
+
+// const nf={
+//     nome:'',
+//     cargo:'',
+//     salário:0,
+// }
+
+// dados.push = prompt('Digite o nome do funcionário: ');
+// dados.push = prompt('Digite o cargo do funcionário: ');
+// dados.push = +prompt('Digite o salário do funcionário: ');
+
+function novof(){ //novo funcionario
+    let dados = [];
+    dados.push = prompt(`Digite o nome do funcionário: `);
+    console.log(dados)
+    dados.unshift = prompt(`Digite o cargo do funcionário: `);console.log(dados)
+    dados.push = +prompt(`Digite o salário do funcionário: `);
+
+    return dados
+    console.log(dados)
+    // let obfuncionario = {
+    //     dados: dados,
+    //     Nome: nomeFun,
+    //     Cargo: cargoFun,
+    //     Salario: salarioFun,
+    // }
+    // console.log(obfuncionario)
+    // return obfuncionario
+
 }
-let teste = nf()
-console.log(teste)
+novof()
+console.log(dados)
+
+// let teste = nf()
+// console.log(teste)
 // let = nf(nnfuncionario)
 // console.log(nnfuncionario)
 // console.log(nf.obfuncionario)
