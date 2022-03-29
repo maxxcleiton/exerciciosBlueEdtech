3a489bb6aa6c471e85afb3b8c7b1810d190b353c
Max Cleiton <maxcleiton@live.com>
2022-03-28 21:24:15 -0300

    Corrigi a chamada, falta arrumar a altura

--- Objetos/Atividade 1.js
+++ Objetos/Atividade 1.js
@@ -5,10 +5,10 @@ console.log();
 // Crie um objeto pessoa com os atributos: nome, idade, peso e altura. Além disso ela deve ter os métodos: Envelhecer, engordar, emagrecer, crescer. Por padrão, a cada ano que nossa pessoa envelhece até os 21 anos, ela deve crescer 0,5 cm. Crie um método bio que retorna uma string com todas as informações da pessoa.
 
 const pessoa = {
-    nome: 'Max',                    
-    idade: 20,
-    peso: 72,
-    altura: 30,
+    nome: '',                    
+    idade: 0,
+    peso: 0,
+    altura: 0,
     
     envelhecer: function(){
         this.idade++
@@ -16,13 +16,20 @@ const pessoa = {
     engordar: function(){
         this.peso++
     },
-    emagrecer: 
+    // emagrecer: 
     // function(quilos){
     //     let pesoperdido = pessoa.peso - quilos
     //     console.log(`A pessoa emagreceu ${pessoa.peso} quilos`)
     //     return pesoperdido
     // },
-    crescer: function(){
+    // crescer: function(){
         
-    },
+    // },
 }
+
+pessoa.nome = prompt('Digite o nome de uma pessoa: ');
+pessoa.idade = +prompt('Digite a idade dela: ');
+pessoa.peso = +prompt('Peso também (em kg): ');
+pessoa.altura = +prompt('E por fim, a altura (em cm): ');
+
+console.log(`Essa pessoa chamada ${pessoa.nome}, tem ${pessoa.idade} anos, e pesa ${pessoa.peso} quilos. Possuindo também a altura até então de ${pessoa.altura}.`)
